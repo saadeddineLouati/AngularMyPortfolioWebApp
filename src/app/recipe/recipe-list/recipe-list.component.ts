b439aad5b466fd441a1f10cb96b9cd7d30865662
saadeddineLouati <saadlouatiii@gmail.com>
2020-01-03 17:56:54 +0100

    dynamic-for-loop

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(`Ionic project`, `Mobile application`, `https://cdn.imgbin.com/18/14/16/imgbin-mobile-app-euclidean-icon-click-the-ppt-phone-material-2Lqgh9BhdFBUtFQMCnEBaKrDq.jpg`),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
